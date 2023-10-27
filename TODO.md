4700e3f6768a3134d54e2e2d9a6c22a9832c9b91
coolmoon327 <535987745@qq.com>
2023-10-27 10:29:46 +0200

    遇到 cuda 使用多进程的问题

--- TODO.md
+++ TODO.md
@@ -2,4 +2,6 @@
 2. 改了 action
 3. 调整了 get_rewards_and_steps 中的 for episode_steps in range(12345), 在测试过程中需要改大 step 上限
 
-reward 好像有点问题?
+修改:
+1. reward 好像有点问题?
+2. PPO 里多进程不能用一个 net, 试着修改 agent
--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from packages.utils.utils import read_config
 from packages.utils.logger import Logger
 from packages.test.test_env import *
 from packages.alg.PPO import train_ppo_for_fogcom
-import multiprocessing
+import multiprocessing as mp
 
 if __name__ == "__main__":
     config = read_config('config.yml')
     
-    manager = multiprocessing.Manager()
+    mp.set_start_method('spawn')
+    manager = mp.Manager()
     result_list = manager.list()
     lock = manager.Lock()
-    threads_num = 16
+    threads_num = 0
     
     # test_env(config)
     train_ppo_for_fogcom(config, threads_num, result_list, lock)
